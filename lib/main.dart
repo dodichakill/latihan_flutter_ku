1311e2184adf7d7c1046ac97121de60c57c04af6
dodichakill <dodiherlangga91@gmail.com>
2021-07-03 00:38:03 -0700

    latihan 2 - text widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,49 @@
-// latihan 1 - hello world
+// latihan 2 - Text widget
 
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('latihan 1'),),
-        body: Center(child: Text("Hello World !")),
+        appBar: AppBar(
+          title: Text('latihan 2'),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 150, 
+                child: Center(
+                    child: Text(
+                  "saya sedang melatih kemampuan flutter saya!",
+                  // maxLines: 2,
+                  // overflow: TextOverflow.fade,
+                  // softWrap: false,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15),
+                      
+                )))),
       ),
     );
   }
 }
 
-
 // penjelasan singkat
 // ----------------
-// untuk membuat sebuah class StatelessWidget secara emmet kita bisa mengetik stl lalu enter
-// untuk membuat sebuah class StatefulWidget secara emmet kita bisa mengetik stf lalu enter
-// sebuah method pada flutter disebut widget
-// Text() merupakan function (widget) untuk menampilkan teks
-// widget Scaffold() merupakan tampilan dasar aplikasi android yang memiliki setidaknya ada 2 bagian utama appbar dan body
-// appbar untuk aplikasi bar
-// body untuk body applikasi
+// untuk merefacor (merapihkan) kode yang terlalu panjang kita bisa menggunakan klik kanan pada kode yang panjang lalu pilih format document atau Shift+Alt+F atau bisa juga dengan menggunakan teknik tulis ulang titik koma (;)
+// widget Center() berfungsi untuk menengahkan konten
+
+// parameter :
+// - maxLines berfungsi untuk memberi batas maksimal baris teks yang ditampilkan
+// - overflow berfungsi untuk mengatur text yang sudah ditentukan batasnya, dan salah satu argumentnya yaitu TextOverflow.ellipsis berfungsi untuk memberi tanda ... untuk text yang melebihi batas, argument TextOverflow.clip untuk memaksa memotong teks yang melebihi batas, argument TextOverflow.fade untuk memberikan efek memudar pada teks yang melebihi batas
+// - softwrap berfungsi untuk memberi wrap, jika argumentnya false maka teksnya tidak akan terwrap (terbungkus) dan akan memotong sesuai ukuran letak teksnya
+// - textAlign berfungsi untuk mengatur tataletak teksnya, argumentnya: TextAlign.center berfungsi untuk menengahkan teks, TextAlign.right dan TextAlign.end untuk merata kanan-kan teks, TextAlign.left dan TextAlign.start untuk merata kriri-kan teks, TextAlign.justify untuk merata kanan-kiri kan teks,
+// - style berfungsi untuk mengatur gaya/styling teks, Argument: widget TextStyle() untuk mewarnai teks menjadi putihdapat berisi beberapa argumen : TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontWeight: FontWeight.w500, fontSize: 15) untuk memberikan warna teks putih, jenis fontnya italic, ketebalan fontnya 500, ukuran fontnya 15
