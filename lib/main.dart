cf557499ee1fc821235711815572cab44f965652
dodichakill <dodiherlangga91@gmail.com>
2021-07-02 23:44:28 -0700

    latihan 1 (finsih)

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+// latihan 1 - hello world
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('latihan 1'),),
+        body: Center(child: Text("Hello World !")),
+      ),
+    );
+  }
+}
+
+
+// penjelasan singkat
+// ----------------
+// untuk membuat sebuah class StatelessWidget secara emmet kita bisa mengetik stl lalu enter
+// untuk membuat sebuah class StatefulWidget secara emmet kita bisa mengetik stf lalu enter
+// sebuah method pada flutter disebut widget
+// Text() merupakan function (widget) untuk menampilkan teks
+// widget Scaffold() merupakan tampilan dasar aplikasi android yang memiliki setidaknya ada 2 bagian utama appbar dan body
+// appbar untuk aplikasi bar
+// body untuk body applikasi
+
+
