8d5d8df4f7fbf0a542132bf9092651cb0d6d740b
dodichakill <dodiherlangga91@gmail.com>
2021-07-06 07:04:19 -0700

    Latihan 16 - AppBar, IconButton, Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-// Latihan 15 - Navigasi Multi Page Screen
+// Latihan 16 - AppBar, IconButton, Icon
 
 import 'package:flutter/material.dart';
-import 'package:latihan_flutter_ku/login_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,15 +9,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+          leading: Icon(
+            Icons.wysiwyg,
+            color: Colors.white,
+          ),
+          title: Text(
+            'Contoh AppBar',
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.settings),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.exit_to_app),
+            ),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.blue, Colors.greenAccent],
+                    begin: FractionalOffset.topLeft,
+                    end: FractionalOffset.bottomRight),
+                image: DecorationImage(
+                    // image: AssetImage('images/texture-kayu.jpg'),
+                    image: AssetImage('images/texture-kayu.png'),
+                    fit: BoxFit.cover)),
+          )),
+      body: Center(
+        child: Container(
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Latihan 16 - AppBar, IconButton, Icon',
+              style: TextStyle(color: Colors.white),
+            )),
+      ),
+    ));
   }
 }
 
 // penjelasan singkat
 // ----------------
-// kita akan membahas konsep navigasi multi pages
-// page/screen di flutter di sebut route, route akan di letakkan di stack. stack akan dipakai untuk menyimpan route. dan yang bisa mengatur stack/route adalah navigator. navigator merupakan yang mengatur perintah yang akan dikerjakan.
-// kita buat ilustrasi sebuah halaman login page, main page, second page. login page berada di stack paling dasar dan main page berada ditengah stack serta second page berada diatas page, dan kita ingin tiap page mempunyai tombol yang mengarah ke page atasnya. dan kita ilustrasikan jika sudah berada di page main page maka kita tidak bisa kembali ke login page, namun second page bisa kembali ke main page. untuk membuat login page tidak bisa diakses kembali setelah memasuki main page kita bisa menggunakan perintah push replacement, dan ketika kita berada di main page ingin memasuki ke second page kita bisa menggunakan perintah push, dan ketika berada di second page kita ingin kembali ke main page kita bisa menggunakan perintah pop.
-// dilatihan ini kita membuat beberapa file untuk melatih kita.
-// langsung saja lihat file terkaitnya supaya lebih paham :)
+// AppBar merupakan widget yang berfungsi untuk memberikan keterangan di bar, keterangan tersebut bisa berupa apa saja seperti widget text maupun widget lainya. AppBar juga memiliki beberapa property seperti:
+// - leading, merupakan property yang akan menampilkan widget disebelah kiri 
+// - title, merupakan property yang akan menampilkan widget disebelah kanan leading
+// - actions,merupakan property yang akan menampilkan widget disebelah kanan title
+// - flexibleSpace, merupakan property yang berguna untuk mengatur background suatu appBar
+// Icon merupakan widget yang berfungsi untuk menampilkan gambar icon dan dapat berisi nilai Icons.(tekan ctrl + spacebar) yang mempunyai berbagai icon yang telah disediakan flutter,
+// ButtonIcon merupakan widget icon yang berguna untuk menjadi tombol, didalamnya juga terdapat porperty onPressed untuk method yang akan dijalankan ketika tombol diklik, dan properti icon yang berisi widget Icon
+// 
