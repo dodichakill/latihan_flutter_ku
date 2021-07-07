c0b3da00193cb4b7fe14dfcee8a402abfe94b1aa
dodichakill <dodiherlangga91@gmail.com>
2021-07-07 08:49:23 -0700

    Latihan 22 - Opacity widget (Custom Card dengan background bercorak)

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,14 @@
-// Latihan 21 - InkWell (membuat sendiri button dengan gradasi)
+// Latihan 22 - Opacity widget (Custom Card dengan background bercorak)
 
 import 'package:flutter/material.dart';
+import 'package:latihan_flutter_ku/main_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Latihan 21 - InkWell (membuat sendiri button dengan gradasi)',
-            style: TextStyle(fontSize: 11),
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              RaisedButton(
-                // berikut merupakan button bawaan
-                onPressed: () {},
-                shape: StadiumBorder(),
-                child: Text('Raised Button'),
-                color: Colors.amber,
-              ),
-              // berikut ini custom button sendiri
-              Material(
-                elevation: 3,
-                borderRadius: BorderRadius.circular(20),
-                child: Container(
-                  width: 150,
-                  height: 44,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      gradient: LinearGradient(
-                          colors: [Colors.green, Colors.blue],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight)),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors
-                        .transparent, // supaya warna dari widget lainya tidak tertimpa
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(20),
-                      onTap: () {},
-                      splashColor:
-                          Colors.red, // untuk memberikan efek sentuh berwarna
-                      child: Center(
-                        child: Text(
-                          'button buatan sendiri',
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: MainPage());
   }
 }
 // penjelasan singkat
