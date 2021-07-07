51caa4eb3b03700cf4733e239d863a4034df2fc1
dodichakill <dodiherlangga91@gmail.com>
2021-07-07 02:50:29 -0700

    Latihan 21 - InkWell (membuat sendiri button dengan gradasi)

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,71 @@
-// Latihan 20 - MediaQuery
+// Latihan 21 - InkWell (membuat sendiri button dengan gradasi)
 
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // TextEditingController controller = TextEditingController(text: 'ini adalah nilai awal textfield');
-  TextEditingController controller = TextEditingController();
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MainPage());
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-          title: Text('Latihan 20 - MediaQuery'),
+          title: Text(
+            'Latihan 21 - InkWell (membuat sendiri button dengan gradasi)',
+            style: TextStyle(fontSize: 11),
+          ),
         ),
-        // body: Container(
-        //   color: Colors.red,
-        //   width: MediaQuery.of(context).size.width /
-        //       3, // berfungsi untuk membuat lebar layar di bagi 3
-        //   height: MediaQuery.of(context).size.height /
-        //       2, // berfungsi untuk membuat lebar layar di bagi 2
-        // ),
-
-        // berikut ini kita akan membuat sebuah tampilan yang responsive
-        body: (MediaQuery.of(context).orientation == Orientation.portrait)
-            ? Column(
-                children: generateContainers,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              RaisedButton(
+                // berikut merupakan button bawaan
+                onPressed: () {},
+                shape: StadiumBorder(),
+                child: Text('Raised Button'),
+                color: Colors.amber,
+              ),
+              // berikut ini custom button sendiri
+              Material(
+                elevation: 3,
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  width: 150,
+                  height: 44,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                          colors: [Colors.green, Colors.blue],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight)),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors
+                        .transparent, // supaya warna dari widget lainya tidak tertimpa
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(20),
+                      onTap: () {},
+                      splashColor:
+                          Colors.red, // untuk memberikan efek sentuh berwarna
+                      child: Center(
+                        child: Text(
+                          'button buatan sendiri',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               )
-            : Row(
-                children: generateContainers,
-              ));
-  }
-
-  List<Widget> get generateContainers {
-    return <Widget>[
-      Container(
-        width: 100,
-        height: 100,
-        color: Colors.red,
+            ],
+          ),
+        ),
       ),
-      Container(
-        width: 100,
-        height: 100,
-        color: Colors.green,
-      ),
-      Container(
-        width: 100,
-        height: 100,
-        color: Colors.blue,
-      ),
-    ];
+    );
   }
 }
 // penjelasan singkat
 // ----------------
-// properti MediaQuery berfungsi untuk menentukan ukuran suatu tinggi dan lebar suatu widget secara flexible
-// diatas merupakan contoh penerapan mediaquery untuk membuat tampilan yang responsive
+// inkWell merupakan widget untuk membuat button sendiri dan memberikan efek sentuhan.
