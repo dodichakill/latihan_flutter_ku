8c604500c485b703a60a607245d87db57d835281
dodichakill <dodiherlangga91@gmail.com>
2021-07-07 01:21:15 -0700

    Latihan 20 - MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// Latihan 19 - TextField (Decoration)
+// Latihan 20 - MediaQuery
 
 import 'package:flutter/material.dart';
 
@@ -14,72 +14,56 @@ class _MyAppState extends State<MyApp> {
   TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Latihan 19 - TextField (Decoration)'),
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(20),
-                child: TextField(
-                  decoration: InputDecoration(
-                    // border: InputBorder.none, // berguna untuk menghilangkan border
-                    // border: OutlineInputBorder(), // berguna untuk membuat border yang mengelilingi fieldnya
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(
-                            10)), // berguna untuk membuat border yang mengelilingi fieldnya dan mencustom border radius sendiri
-                    icon: Icon(Icons
-                        .adb), // berguna untuk membuat icon disebelah kiri field
-                    prefixIcon: Icon(Icons
-                        .person), // berguna untuk membuat icon didalam field pada sebelah kiri
-                    suffixIcon: Icon(Icons
-                        .search), // berguna untuk membuat icon didalam field pada sebelah kanan
-                    prefix: Container(
-                      width: 10,
-                      height: 10,
-                      margin: EdgeInsets.only(right: 10),
-                      color: Colors.green,
-                    ), // berguna untuk bisa memuat widget lainya disini contohnya menggunakan widget container
-                    suffix: Container(
-                      width: 10,
-                      height: 10,
-                      margin: EdgeInsets.only(left: 10),
-                      color: Colors.red,
-                    ), // berguna untuk bisa memuat widget lainya disini contohnya menggunakan widget container
-                    // prefixText: 'nama : ', // berguna untuk membuat text didalam field sebelah kiri
-                    // suffixText: 'bin toyib', // berguna untuk membuat text didalam field sebelah kiri
-                    // prefixStyle: TextStyle( color: Colors.blue, fontWeight: FontWeight.bold), // berguna untuk membuat styling untuk prefixText
-                    // suffixStyle: TextStyle(color: Colors.green,fontWeight: FontWeight.bold), // berguna untuk membuat styling untuk Suffixtext
-                    labelText:
-                        "masukan namamu", // berguna untuk membuat text diatas field dalam bordernya
-                    labelStyle: TextStyle(
-                        color: Colors.red), // berguna untuk styling label
-                    hintText:
-                        'ketik di sini...', // berguna untuk membuat text placeholder
-                    hintStyle: TextStyle(
-                        color: Colors.blue), // berguna untuk styling hint
-                    filled:
-                        true, // berguna untuk memperbolehkan fieldnya berwarna
-                    fillColor: Colors
-                        .lightBlue[100], // berguna untuk memberi warna fieldnya
-                  ),
-                  onChanged: (value) {
-                    setState(() {});
-                  },
-                  controller: controller,
-                ),
-              ),
-              Text(controller.text)
-            ],
-          )),
-    );
+    return MaterialApp(home: MainPage());
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Latihan 20 - MediaQuery'),
+        ),
+        // body: Container(
+        //   color: Colors.red,
+        //   width: MediaQuery.of(context).size.width /
+        //       3, // berfungsi untuk membuat lebar layar di bagi 3
+        //   height: MediaQuery.of(context).size.height /
+        //       2, // berfungsi untuk membuat lebar layar di bagi 2
+        // ),
+
+        // berikut ini kita akan membuat sebuah tampilan yang responsive
+        body: (MediaQuery.of(context).orientation == Orientation.portrait)
+            ? Column(
+                children: generateContainers,
+              )
+            : Row(
+                children: generateContainers,
+              ));
+  }
+
+  List<Widget> get generateContainers {
+    return <Widget>[
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.red,
+      ),
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.green,
+      ),
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.blue,
+      ),
+    ];
   }
 }
 // penjelasan singkat
 // ----------------
-// widget TextField memiliki property InputDecoration yang memiliki berbagai widget juga property yang berguna untuk memodifikasi tampilan texfield. lihat saja pada kode diatas
-// jika kita menggunakan property prefix maka property prefixText dan prefixStyle harus di nonaktifkan terlebih dahulu
-// jika kita menggunakan property Suffix maka property SuffixText dan SuffixStyle harus di nonaktifkan terlebih dahulu
+// properti MediaQuery berfungsi untuk menentukan ukuran suatu tinggi dan lebar suatu widget secara flexible
+// diatas merupakan contoh penerapan mediaquery untuk membuat tampilan yang responsive
