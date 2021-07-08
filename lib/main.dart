c64c16f865a4a7f4c0ad817a2df8e28b56c34b39
dodichakill <dodiherlangga91@gmail.com>
2021-07-08 01:48:59 -0700

    Latihan 26 - TabBar, TabBarView widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// Latihan 24 - Hero, ClipRRect widget
+// Latihan 26 - TabBar, TabBarView widget
 
 import 'package:flutter/material.dart';
 
@@ -8,67 +8,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false, //menghilangkan banner debug
-        home: Scaffold(
-          // appbar biasa
-          // appBar: AppBar(
-          //   backgroundColor: Colors.green,
-          //   title: Text(
-          //     'Latihan 25 - PrefferedSized widget',
-          //     style: TextStyle(fontSize: 11),
-          //   ),
-          // ),
-
-          // // appBar custom height
-          // appBar: PreferredSize(
-          //   // PreferredSize berguna untuk mengcustom tampilan appbar
-          //   preferredSize: Size.fromHeight(200),
-          //   child: AppBar(
-          //     backgroundColor: Colors.indigo,
-          //     title: Text('appbar Custom height'),
-          //   ),
-          // ),
-
-          // appBar custom layout dengan FlexibleSpace
-          appBar: PreferredSize(
-            // PreferredSize berguna untuk mengcustom tampilan appbar
-            preferredSize: Size.fromHeight(200),
-            child: AppBar(
-              backgroundColor: Colors.indigo,
-              flexibleSpace: Container(
-                margin: EdgeInsets.all(20),
-                child: Positioned(
-                  bottom: 100,
-                  right: 10000,
-                  child: Text(
-                    'Custom appbar dengan eflexibleSpace',
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.yellow,
-                        fontWeight: FontWeight.w700),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Latihan 26 - TabBar, TabBarView widget',
+              style: TextStyle(fontSize: 15.5),
+            ),
+            bottom: TabBar(
+              //untuk memberikan tombol pada app bar
+              tabs: [
+                //berisi beberapa tab dan isi dari tabnya bebas
+                Tab(
+                  icon: Icon(Icons.album_rounded),
+                  text: ('album'),
+                ),
+                Tab(
+                  child: Image(
+                    image: AssetImage('images/fb.png'),
                   ),
                 ),
-              ),
-            ),
-          ),
-
-          body: Center(
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.circular(10)),
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                'Latihan 25 - PrefferedSized widget (Membuat Custom AppBar dengan Custom Height)',
-                style: TextStyle(
-                  color: Colors.white,
+                Tab(icon: Icon(Icons.computer)),
+                Tab(
+                  child: Text('News'),
                 ),
-                textAlign: TextAlign.center,
-              ),
+              ],
             ),
           ),
-        ));
+          body: TabBarView(
+            //untuk menambahkan data pada setiap tab dan dapat langsung dilihat
+            children: [
+              Center(
+                child: Text('isi data dari tab 1'),
+              ),
+              Center(
+                child: Text('isi data dari tab 2'),
+              ),
+              Center(
+                child: Text('isi data dari tab 3'),
+              ),
+              Center(
+                child: Text('isi data dari tab 4'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 // penjelasan singkat
