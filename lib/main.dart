384e8b4dbe73eabd55383cca7148ad77f8541872
dodichakill <dodiherlangga91@gmail.com>
2021-07-07 23:50:42 -0700

    Latihan 24 - Hero, ClipRRect widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
-// Latihan 23 - Positioned, FloatingActionButton widget
+// Latihan 24 - Hero, ClipRRect widget
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:latihan_flutter_ku/main_page.dart';
 
-void main() {
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
-      (_) => runApp(MyApp())); //untuk mencegah aplikasi berorientasi landscape
-}
+void main() => runApp(MyApp()); //untuk mencegah aplikasi berorientasi landscape
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false, //menghilangkan banner debug
+        //menghilangkan banner debug
         home: MainPage());
   }
 }
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,213 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_flutter_ku/second_page.dart';
 
 class MainPage extends StatelessWidget {
-  double getSmallDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 2 / 3;
-  double getBigDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 7 / 8;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[200],
         appBar: AppBar(
           title: Text(
-            'Latihan 23 - Positioned, FloatingActionButton widget',
-            style: TextStyle(fontSize: 12),
+            'Latihan 24 - Hero, ClipRRect widget',
+            style: TextStyle(fontSize: 14),
           ),
         ),
-        body: Stack(
-          children: <Widget>[
-            Positioned(
-              // positioned berfungsi untuk memberikan custom posisi
-              right: -getSmallDiameter(context) / 3,
-              top: -getSmallDiameter(context) / 3,
+        body: GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SecondPage();
+            }));
+          },
+          // berikut merupakan tanpa animasi default(saat ditekan)
+          // child: ClipRRect(
+          //   // berfungsi untuk membuat kotak yang dapat di rounded/bulatkan/radius
+          //   borderRadius: BorderRadius.circular(75),
+          //   child: Container(
+          //     width: 150,
+          //     height: 150,
+          //     child: Image(
+          //       fit: BoxFit.cover,
+          //       image: AssetImage('images/img.jpg'),
+          //     ),
+          //   ),
+          // ),
+
+          // berikut animasi versi custom/hero (yaitu animasi yang seolah timbul/keluar dari objeknya)
+          child: Hero(
+            // hero berfungsi untuk memberikan animasi transisi pada objek
+            tag:
+                'contoh', // tag berfungsi sebagai penanda untuk objek yang akan dianimasikan (nama tagnya harus sama dengan tag objeknya)
+            child: ClipRRect(
+              // berfungsi untuk membuat kotak yang dapat di rounded/bulatkan/radius
+              borderRadius: BorderRadius.circular(75),
               child: Container(
-                width: getSmallDiameter(context),
-                height: getSmallDiameter(context),
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle, // untuk memberikan shape bulat
-                    gradient: LinearGradient(
-                        colors: [Colors.purpleAccent, Colors.blue[400]],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter)),
-              ),
-            ),
-            Positioned(
-              // positioned berfungsi untuk memberikan custom posisi
-              left: -getBigDiameter(context) / 4,
-              top: -getBigDiameter(context) / 4,
-              child: Container(
-                child: Center(
-                  child: Text(
-                    'Dribble',
-                    style: TextStyle(
-                        fontFamily: 'Lobster',
-                        fontSize: 30,
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white),
-                  ),
-                ),
-                width: getBigDiameter(context),
-                height: getBigDiameter(context),
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle, // untuk memberikan shape bulat
-                    gradient: LinearGradient(
-                        colors: [Colors.purpleAccent, Colors.blue],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter)),
-              ),
-            ),
-            Positioned(
-              // positioned berfungsi untuk memberikan custom posisi
-              right: -getBigDiameter(context) / 2,
-              bottom: -getBigDiameter(context) / 2,
-              child: Container(
-                width: getBigDiameter(context),
-                height: getBigDiameter(context),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle, // untuk memberikan shape bulat
-                  color: Colors.grey[300],
+                width: 150,
+                height: 150,
+                child: Image(
+                  fit: BoxFit.cover,
+                  image: AssetImage('images/texture-kayu.jpg'),
                 ),
               ),
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: ListView(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(5)),
-                    margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
-                    child: Column(
-                      children: [
-                        TextField(
-                          decoration: InputDecoration(
-                              icon: Icon(
-                                Icons.email,
-                                color: Color(0xFFFF4891),
-                              ),
-                              focusedBorder: UnderlineInputBorder(
-                                  borderSide:
-                                      BorderSide(color: Color(0xFFFF4891))),
-                              labelText: 'Email :',
-                              labelStyle: TextStyle(color: Color(0xFFFF4891))),
-                        ),
-                        TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                              icon: Icon(
-                                Icons.vpn_key,
-                                color: Color(0xFFFF4891),
-                              ),
-                              focusedBorder: UnderlineInputBorder(
-                                  borderSide:
-                                      BorderSide(color: Color(0xFFFF4891))),
-                              labelText: 'Password :',
-                              labelStyle: TextStyle(color: Color(0xFFFF4891))),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
-                      child: Text(
-                        'FORGOT PASSWORD?',
-                        style:
-                            TextStyle(color: Color(0xFFFF4891), fontSize: 11),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(20, 0, 20, 30),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.4,
-                          height: 40,
-                          child: Container(
-                            child: Material(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.transparent,
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(20),
-                                splashColor: Colors.lightBlue,
-                                onTap: () {},
-                                child: Center(
-                                  child: Text(
-                                    'Sign In',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                gradient: LinearGradient(
-                                    colors: [Colors.purpleAccent, Colors.blue],
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter)),
-                          ),
-                        ),
-                        FloatingActionButton(
-                          // berguna untuk membuat button yang melayang
-                          elevation: 0,
-                          mini: true, // supaya ukurannya diperkecil
-                          onPressed: () {},
-                          child: Image(
-                            image: AssetImage('images/fb.png'),
-                          ),
-                        ),
-                        FloatingActionButton(
-                          elevation: 0,
-                          mini: true,
-                          onPressed: () {},
-                          child: Image(
-                            image: AssetImage('images/gl.png'),
-                          ),
-                        ),
-                        FloatingActionButton(
-                          elevation: 0,
-                          mini: true,
-                          onPressed: () {},
-                          child: Image(
-                            image: AssetImage('images/tw.png'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "DON'T HAVE AN ACCOUNT? ",
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 11),
-                      ),
-                      Text(
-                        "SIGN UP",
-                        style: TextStyle(
-                            color: Color(0xFFFF4891),
-                            fontWeight: FontWeight.w500,
-                            fontSize: 11),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
+          ),
         ));
   }
 }
