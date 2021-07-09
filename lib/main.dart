e28da637228e71d189d6bfb0f5bb8af399147c9f
dodichakill <dodiherlangga91@gmail.com>
2021-07-08 23:18:27 -0700

    Latihan 30 - Gradient Opacity (transparansi bergradasi)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-// Latihan 29 - Transform widget (membuat custom button belah ketupat)
+// Latihan 30 - Gradient Opacity (transparansi bergradasi)
 
 import 'package:flutter/material.dart';
-import 'package:latihan_flutter_ku/colorful_button.dart';
 
 void main() => runApp(MyApp()); //untuk mencegah aplikasi berorientasi landscape
 
@@ -10,40 +9,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Latihan 29 - Transform Widget'),
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.purple,
-                ),
-                padding: EdgeInsets.all(15),
-                child: Text(
-                  'membuat custom button belah ketupat',
-                  style: TextStyle(color: Colors.white),
-                )),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ColorfulButton(Colors.red, Colors.blue, Icons.adb),
-                ColorfulButton(Colors.amber, Colors.green, Icons.comment),
-                ColorfulButton(Colors.green, Colors.purple, Icons.computer),
-                ColorfulButton(Colors.blue, Colors.red, Icons.account_box),
-              ],
+          appBar: AppBar(
+            title: Text('Latihan 30 - Gradient Opacity'),
+          ),
+          body: Center(
+            child: ShaderMask(
+              shaderCallback: (rectangle) {
+                return LinearGradient(
+                  colors: [Colors.black, Colors.transparent],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ).createShader(
+                    Rect.fromLTRB(0, 0, rectangle.width, rectangle.height));
+                //berguna untuk membuat efek gradasi opacity linear dengan mask
+              },
+              blendMode:
+                  BlendMode.dstIn, //untuk membuat gambarnya menyatu dengan mask
+              child: Image(
+                width: 300,
+                image: AssetImage('images/img.jpg'),
+              ),
             ),
-          ],
-        )),
-      ),
+          )),
     );
   }
 }
 
 // penjelasan singkat
 // ----------------
-// Widget Transform berguna untuk merubah suatu posisi objek. 
-// lihat penjelasan lainya di codenya
+// Widget ShaderMask berguna untuk mengolah gambar menjadi masking dengan berbagai pilihan seperti membuatnya gradasi opacity seperti contoh diatas
