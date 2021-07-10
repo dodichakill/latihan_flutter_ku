5d02d72e74df995de4872a5b1b0e1ef3d4cff9f8
dodichakill <dodiherlangga91@gmail.com>
2021-07-10 07:49:19 -0700

    Latihan 35 - HTTP Request (POST)

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,57 @@
-// Latihan 34 - ClipPath
+// Latihan 35 - HTTP Request (POST)
 
 import 'package:flutter/material.dart';
+import 'package:latihan_flutter_ku/post_result_model.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  MyPostResult myPostResult = null; //nilai awal
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Latihan 34 - ClipPath'),
+          title: Text('Latihan 35 - HTTP Request (POST)'),
         ),
         body: Center(
-          child: ClipPath(
-            clipper: MyClipper(),
-            child: Image(
-              width: 300,
-              image: AssetImage('images/img2.jpg'),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              (myPostResult != null)
+                  ? 'id : ${myPostResult.id} \n  name : ${myPostResult.name} \n  job : ${myPostResult.job} \n date : ${myPostResult.created}'
+                  : "tidak ada data", // jika datanya berhasil terkirim maka akan memunculkan data dari server dan jika tidak berhasil maka akan menampilkan pesan tidak ada ada
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 19,
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.center,
+              maxLines: 4,
             ),
-          ),
-        ),
+            ElevatedButton(
+                onPressed: () {
+                  MyPostResult.connectToAPI('dodi', 'programmer').then((value) {
+                    myPostResult = value;
+                    setState(() {});
+                  }); // disini kita memanggil API dan menambahkan sebuah data yang nanti akan kita tampilkan
+                },
+                child: Text('POST'))
+          ],
+        )),
       ),
     );
   }
 }
-
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    Path path = Path();
-
-    // disini kita custom clipnya
-    path.lineTo(0, size.height);
-    path.quadraticBezierTo(
-        size.width / 2, size.height * 0.75, size.width, size.height);
-    path.lineTo(size.width, 0);
-    path.close();
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
-}
-
 // penjelasan singkat
 // ----------------
-// diatas merupakan contoh cara penggunannya dan penjelesananya
+// sebelumnya kita perlu memasang http pada pubspec.yaml
+// pada contoh kali ini kita menggunakan API dari reqres.in untuk mempermudah pembnelajaran kita kali ini
+// diatas merupakan contoh cara penggunannya dan beberapa penjelesannya
