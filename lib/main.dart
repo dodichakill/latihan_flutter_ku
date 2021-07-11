012933be157429cef013cfa26f2bd26d68f4b7a1
dodichakill <dodiherlangga91@gmail.com>
2021-07-11 02:32:40 -0700

    Latihan 38 - Switch & AnimatedSwitcher widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-// Latihan 37 - HTTP Request (GET) banyak data sekaligus
+// Latihan 38 - Switch & AnimatedSwitcher widget
 
 import 'package:flutter/material.dart';
-import 'package:latihan_flutter_ku/get_users_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,44 +10,122 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String output = 'no data'; //nilai awal
-
+  bool isON = false;
+  Widget myWidget = Container(
+      width: 200,
+      height: 100,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 5),
+          color: Colors.red));
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Latihan 37 - HTTP Request (GET) banyak data sekaligus',
+          title: Text('Latihan 38 - Switch & AnimatedSwitcher widget',
               style: TextStyle(
-                fontSize: 12,
+                fontSize: 15,
               )),
         ),
         body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text(
-              output,
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 19,
-                fontWeight: FontWeight.w600,
+            AnimatedSwitcher(
+              duration: Duration(seconds: 1),
+              child: myWidget,
+              // transitionBuilder: (child, animation) => ScaleTransition(
+              //   scale: animation,
+              //   child: child,
+              // ),
+              transitionBuilder: (child, animation) => RotationTransition(
+                turns: animation,
+                child: child,
               ),
-              textAlign: TextAlign.center,
-              maxLines: 4,
             ),
-            ElevatedButton(
-                onPressed: () {
-                  User.getUsers('2').then((users) {
-                    output = '';
-                    for (int i = 0; i < users.length; i++) {
-                      output += '[ nama user: ${users[i].name} ]';
+            Switch(
+                activeColor: Colors.green, //memberikan warna ketika aktif/on
+                inactiveTrackColor: Colors
+                    .red[200], //memberikan warna pada track ketika keadaan off
+                inactiveThumbColor: Colors
+                    .red, //memberikan warna pada thumb/tombol ketika keadaan off
+                value: isON,
+                // berikut contoh perubahan animasi dari container ke text
+                // onChanged: (newValue) {
+                //   isON = newValue;
+                //   setState(() {
+                //     if (isON) {
+                //       myWidget = SizedBox(
+                //         height: 100,
+                //         width: 200,
+                //         child: Center(
+                //           child: Text(
+                //             'switch: ON',
+                //             style: TextStyle(
+                //               color: Colors.green,
+                //               fontWeight: FontWeight.w700,
+                //               fontSize: 20,
+                //             ),
+                //             textAlign: TextAlign.center,
+                //           ),
+                //         ),
+                //       );
+                //     } else {
+                //       myWidget = Container(
+                //           width: 200,
+                //           height: 100,
+                //           decoration: BoxDecoration(
+                //               border: Border.all(color: Colors.black, width: 5),
+                //               color: Colors.red));
+                //     }
+
+                // dibawah ini merupakan contoh perubahan dari container ke container dan tanpa key
+                // onChanged: (newValue) {
+                //   isON = newValue;
+                //   setState(() {
+                //     if (isON) {
+                //       myWidget = Container(
+                //           width: 200,
+                //           height: 100,
+                //           decoration: BoxDecoration(
+                //               border: Border.all(
+                //                   color: Colors.green[200], width: 5),
+                //               color: Colors.green));
+                //     } else {
+                //       myWidget = Container(
+                //           width: 200,
+                //           height: 100,
+                //           decoration: BoxDecoration(
+                //               border:
+                //                   Border.all(color: Colors.red[200], width: 5),
+                //               color: Colors.red));
+                //     }
+                //   });
+                // dibawah ini merupakan contoh perubahan dari container ke container dan menggunakan key
+                onChanged: (newValue) {
+                  isON = newValue;
+                  setState(() {
+                    if (isON) {
+                      myWidget = Container(
+                          key: ValueKey(1),
+                          width: 200,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.green[200], width: 5),
+                              color: Colors.green));
+                    } else {
+                      myWidget = Container(
+                          key: ValueKey(2),
+                          width: 200,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.red[200], width: 5),
+                              color: Colors.red));
                     }
                   });
-                  setState(() {});
-                  // disini kita memanggil API dan mengambil data dari id 4
-                },
-                child: Text('Get'))
+                })
           ],
         )),
       ),
@@ -57,6 +134,6 @@ class _MyAppState extends State<MyApp> {
 }
 // penjelasan singkat
 // ----------------
-// sebelumnya kita perlu memasang http pada pubspec.yaml
-// pada contoh kali ini kita menggunakan API dari reqres.in untuk mempermudah pembnelajaran kita kali ini
-// diatas merupakan contoh cara penggunannya dan beberapa penjelesannya
+// widget Switch berfungsi untuk memberikan tombol switch(on/off toggle)
+// widget AnimatedSwitcher berfungsi untuk memberikan animasi kepada widget Switch
+// diatas merupakan contoh penggunaanya
